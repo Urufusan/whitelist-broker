b138b52727d02ddb67b4591157beaa941ecf6d57
Urufusan <63711492+Urufusan@users.noreply.github.com>
2024-04-03 03:33:17 +0200

    fix!

--- craftycmdsender.py
+++ craftycmdsender.py
@@ -44,6 +44,9 @@ token = os.environ.get("CRAFTY_TOKEN")
 # command = "mem"
 
 for h in get_player_whitelist():
-    uuid_spec = requests.get(f"https://api.minecraftservices.com/minecraft/profile/lookup/name/{h['name']}").json()
-    send_stdin_command(server_id, token, f"lpv user {uuid_spec['id']} permission set serverpermissions.server.xairencraft")
+    try:
+        uuid_spec = requests.get(f"https://api.minecraftservices.com/minecraft/profile/lookup/name/{h['name']}").json()
+        send_stdin_command(server_id, token, f"lpv user {uuid_spec['id']} permission set serverpermissions.server.xairencraft")
+    except:
+        time.sleep(5)
     time.sleep(0.5)
